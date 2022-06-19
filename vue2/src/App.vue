e1d7965924639740a8ec6ae49c25a912bfe086fa
wangyan-workspace <2107014530@qq.com>
2022-06-19 23:08:55 +0800

    vue2子应用新能源页面实现

---
+++ vue2/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app-vue   1">
+    <!-- 作为入口使用 -->
+    <router-view></router-view>
+  </div>
+</template>
+<script>
+export default {
+  name: 'App',
+}
+</script>
+<style>
+</style>
