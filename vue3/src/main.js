ebf2f293c04087f2ac0d40f3cb33070bd2e012a6
wangyan-workspace <2107014530@qq.com>
2022-06-20 09:33:34 +0800

    子应用vue3项目实现完成

---
+++ vue3/src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+//引入router
+import router from './router';
+
+createApp(App).use(router).mount('#app');
+
+
---
+++ vue3/src/pages/index/index.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="index-container">
+    <div class="index-content">
+      <NavTitle />
+      <IndexAd />
+      <CarSwapper />
+      <IndexAd />
+      <NewsList />
+    </div>
+    <!-- <div @click="changeNum">
+        {{ num }}
+    </div> -->
+  </div>
+</template>
+
+<script>
+import { main } from "../../utils/global";
+import NavTitle from "./components/navTitle.vue";
+import IndexAd from "./components/ad.vue";
+import CarSwapper from "./components/carSwapper.vue";
+import NewsList from "./components/newsList.vue";
+
+// import { ref } from 'vue';
+
+export default {
+  name: "index",
+  components: {
+    NavTitle,
+    IndexAd,
+    CarSwapper,
+    NewsList,
+  },
+  setup() {
+    // 创建响应式的数据
+    // const num = ref(0);
+    // console.log(num);
+
+    // const changeNum = () => {
+    //     num.value++
+    // }
+    if (main.appInfo) {
+      main.appInfo.crumbsState.setCrumbs([])
+    }
+    // return {
+    //     num,
+    //     changeNum
+    // }
+  },
+};
+</script>
+
+<style lang="scss">
+.index {
+  &-container {
+    width: 100%;
+  }
+  &-content {
+    width: 1200px;
+    margin: 40px auto 0;
+  }
+}
+</style>
